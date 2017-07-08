351de04f6c796fc3f64c9bee611d8b079ad31c53
venconi91 <vencislav_911115@abv.bg>
2017-07-08 18:40:49 +0300

    feat: functionality for checking connection

--- index.js
+++ index.js
@@ -3,7 +3,5 @@ const manager = require('./lib');
 module.exports = {
     disableConnection: manager.disableConnection,
     enableConnection: manager.enableConnection,
-    isEnabled: () => {
-        throw new Error('not implemented');
-    }
+    isConnected: manager.isConnected
 }
